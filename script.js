707faa9240eb5077161831ae5d76b1bb4b8a16be
Artem Hlezin <21369363+artemhlezin@users.noreply.github.com>
2022-04-15 20:17:48 +0300

    Init commit

---
+++ script.js
@@ -0,0 +1,4 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { FullScreenQuad } from "three/examples/jsm/postprocessing/Pass.js";
+
